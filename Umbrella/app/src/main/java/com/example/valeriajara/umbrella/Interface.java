d331de0b99210beeb2f94062f3fcf8b477bc43cb
Micah Rowell <m_r525@txstate.edu>
2017-09-24 20:13:09 -0500

    Firebase added

--- Umbrella/app/src/main/java/com/example/valeriajara/umbrella/Interface.java
+++ Umbrella/app/src/main/java/com/example/valeriajara/umbrella/Interface.java
@@ -2,7 +2,6 @@ package com.example.valeriajara.umbrella;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 public class Interface extends AppCompatActivity {
 
     @Override
