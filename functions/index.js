5754b5cef9dfca01e388483550c27fc94b987a0e
Micah Rowell <m_r525@txstate.edu>
2017-11-07 12:47:12 -0600

    Deleting unnecessary things

--- functions/index.js
+++
@@ -1,6 +0,0 @@
-var functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
-functions.auth.user().onCreate(event => {
-	admin.database().ref('users/' + user.uid).set(userObject);
-});
